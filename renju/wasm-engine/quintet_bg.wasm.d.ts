a4546aa0e084dec3829c92173e82147b95cbfebd
renju4433 <yaobf17041@gmail.com>
2025-12-12 18:43:16 +0800

    Add files via upload

---
+++ renju/wasm-engine/quintet_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function solve(a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number): void;
+export function solve_vcf(a: number, b: number, c: number, d: number, e: number, f: number, g: number): void;
+export function solve_vct(a: number, b: number, c: number, d: number, e: number, f: number, g: number): void;
+export function solve_vct_dfpn(a: number, b: number, c: number, d: number, e: number, f: number, g: number): void;
+export function encode_xy(a: number, b: number): number;
+export function decode_x(a: number): number;
+export function decode_y(a: number): number;
+export function engine_search(a: number, b: number, c: number, d: number, e: number, f: number, g: number): number;
+export function engine_evaluate(a: number, b: number, c: number, d: number, e: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
