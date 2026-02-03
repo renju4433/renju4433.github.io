f1781c3f8f34cbd4165ded82aa34b4274eb0f6e2
renju4433 <yaobf17041@gmail.com>
2026-02-03 12:24:50 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.c5dbaddd9a84ccfcdffa4e23aeabeaa2.js"
+  "precache-manifest.a0e69989a4b39aba6ad3520e60d2cf00.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "gomoku-calculator"});
