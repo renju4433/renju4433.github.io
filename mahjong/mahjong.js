ad62e8fa8b392d193c8c0da2a752d1444bc75d53
renju4433 <yaobf17041@gmail.com>
2026-09-19 11:48:09 +0800

    Add files via upload

--- mahjong/mahjong.js
+++ mahjong/mahjong.js
@@ -1,6 +1,6 @@
 function parseHand(str) {
   var counts = new Array(34);
-  for(var i=0; i<34; i++) counts[i] = 0;
+  for (var i = 0; i < 34; i++) counts[i] = 0;
   var nums = [];
   var count = 0;
   var tiles = [];
@@ -15,16 +15,18 @@ function parseHand(str) {
       else if (c === 'p') offset = 9;
       else if (c === 's') offset = 18;
       else if (c === 'z') offset = 27;
-      
+
       if (offset !== -1) {
         for (var j = 0; j < nums.length; j++) {
+          var isZero = (str[i - nums.length + j] === '0');
+          if (offset === 27 && isZero) continue;
           if (offset === 27 && nums[j] > 6) continue;
           var t = offset + nums[j];
           counts[t]++;
           count++;
-          
+
           var isAka = false;
-          if (offset !== 27 && nums[j] === 4 && str[i - nums.length + j] === '0') {
+          if (offset !== 27 && nums[j] === 4 && isZero) {
             isAka = true;
             akas[t] = (akas[t] || 0) + 1;
           }
@@ -43,38 +45,41 @@ function initTable(data) {
   suitTable = data;
 }
 
-function getShanten(counts, count) {
+function getShanten(counts, count, type) {
   var minS = 8;
-  var kCount = 0, kPair = 0;
-  var kTiles = [0,8,9,17,18,26,27,28,29,30,31,32,33];
-  for (var i = 0; i < kTiles.length; i++) {
-    if (counts[kTiles[i]] > 0) {
-      kCount++;
-      if (counts[kTiles[i]] > 1) kPair = 1;
+
+  if (type !== 'standard') {
+    var kCount = 0, kPair = 0;
+    var kTiles = [0, 8, 9, 17, 18, 26, 27, 28, 29, 30, 31, 32, 33];
+    for (var i = 0; i < kTiles.length; i++) {
+      if (counts[kTiles[i]] > 0) {
+        kCount++;
+        if (counts[kTiles[i]] > 1) kPair = 1;
+      }
     }
-  }
-  if (count >= 13) {
-    minS = Math.min(minS, 13 - kCount - kPair);
-  }
-  
-  var pairs = 0, kinds = 0;
-  for (var i = 0; i < 34; i++) {
-    if (counts[i] > 0) kinds++;
-    if (counts[i] >= 2) pairs++;
-  }
-  if (count >= 13) {
-    minS = Math.min(minS, 6 - pairs + Math.max(0, 7 - kinds));
+    if (count >= 13) {
+      minS = Math.min(minS, 13 - kCount - kPair);
+    }
+
+    var pairs = 0, kinds = 0;
+    for (var i = 0; i < 34; i++) {
+      if (counts[i] > 0) kinds++;
+      if (counts[i] >= 2) pairs++;
+    }
+    if (count >= 13) {
+      minS = Math.min(minS, 6 - pairs + Math.max(0, 7 - kinds));
+    }
   }
 
   var targetMelds = Math.floor(count / 3);
 
   function getSuitDp(d_start, d_end, isHonor) {
-    var dp = [ [-1,-1,-1,-1,-1], [-1,-1,-1,-1,-1] ];
+    var dp = [[-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1]];
     var key = 0;
     for (var i = d_start; i < d_end; i++) {
       key = key * 5 + counts[i];
     }
-    
+
     if (isHonor) {
       function dfs_honor(d, melds, blocks, pair) {
         while (d < d_end && counts[d] === 0) d++;
@@ -83,9 +88,9 @@ function getShanten(counts, count) {
           return;
         }
         var c = counts[d];
-        if (c >= 3) { counts[d]-=3; dfs_honor(d, melds+1, blocks, pair); counts[d]+=3; }
-        if (c >= 2 && !pair) { counts[d]-=2; dfs_honor(d, melds, blocks, 1); counts[d]+=2; }
-        if (c >= 2) { counts[d]-=2; dfs_honor(d, melds, blocks+1, pair); counts[d]+=2; }
+        if (c >= 3) { counts[d] -= 3; dfs_honor(d, melds + 1, blocks, pair); counts[d] += 3; }
+        if (c >= 2 && !pair) { counts[d] -= 2; dfs_honor(d, melds, blocks, 1); counts[d] += 2; }
+        if (c >= 2) { counts[d] -= 2; dfs_honor(d, melds, blocks + 1, pair); counts[d] += 2; }
         dfs_honor(d + 1, melds, blocks, pair);
       }
       dfs_honor(d_start, 0, 0, 0);
@@ -95,7 +100,7 @@ function getShanten(counts, count) {
     if (!suitTable) {
       throw new Error("Table not loaded yet");
     }
-    
+
     var val = suitTable[key] || 0;
     for (var p = 0; p <= 1; p++) {
       for (var m = 0; m <= 4; m++) {
@@ -106,17 +111,17 @@ function getShanten(counts, count) {
     }
     return dp;
   }
-  
+
   var resMan = getSuitDp(0, 9, false);
   var resPin = getSuitDp(9, 18, false);
   var resSou = getSuitDp(18, 27, false);
   var resHon = getSuitDp(27, 34, true);
 
-  var dp = [ [-1,-1,-1,-1,-1], [-1,-1,-1,-1,-1] ];
+  var dp = [[-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1]];
   dp[0][0] = 0;
 
   function merge(res) {
-    var next_dp = [ [-1,-1,-1,-1,-1], [-1,-1,-1,-1,-1] ];
+    var next_dp = [[-1, -1, -1, -1, -1], [-1, -1, -1, -1, -1]];
     for (var p1 = 0; p1 <= 1; p1++) {
       for (var m1 = 0; m1 <= 4; m1++) {
         if (dp[p1][m1] === -1) continue;
@@ -153,6 +158,6 @@ function getShanten(counts, count) {
     }
   }
 
-  minS = Math.min(minS, normalMinS);
-  return minS;
+  if (type === 'standard') return normalMinS;
+  return Math.min(minS, normalMinS);
 }
